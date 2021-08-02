950d05a9367df1c1c9e7682fa4d55a2cbcf630e0
MZ7-CR7 <88025074+MZ7-CR7@users.noreply.github.com>
2021-08-02 16:31:30 +0530

    commit

--- Activity_04.py
+++ Activity_04.py
@@ -1,4 +1,13 @@
 a=int(input("enter the first number to add:"))
 b=int(input("enter the second number to add:"))
 z=a+b
-print("The sum of %d + %d = %d \n"%(a,b,z))
+#c script method
+print("The sum of \n%d + %d = %d \n"%(a,b,z))
+#end method
+print(a,end=" + ")
+print(b,end=" =")
+print("",z)
+#.format method
+print("{0} + {1} = {2}".format(a,b,z))
+#f-string method
+print(f"The sum of\n{a} + {b} = {z}")
