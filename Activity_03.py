bcca9035e37aaf9e80992c8aa55ddaf1ef598371
MZ7-CR7 <88025074+MZ7-CR7@users.noreply.github.com>
2021-08-02 15:25:45 +0530

    commit

--- Activity_03.py
+++ Activity_03.py
@@ -1,6 +1,4 @@
 m=input("enter string 1 :")
 n=input("enter string 2 :")
-concat_strings=m+' '+n
-print(concat_strings)
-for i in range(5):
-    print("\n" ,concat_strings )
+concat_strings=m+' '+n + "\n" 
+print(concat_strings *5)
