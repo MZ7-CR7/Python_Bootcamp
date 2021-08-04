fe1d86648e72b7ca5582b1c960707876d850a6c0
MZ7-CR7 <88025074+MZ7-CR7@users.noreply.github.com>
2021-08-04 14:58:45 +0530

    commit

---
+++ Activity_11.py
@@ -0,0 +1,17 @@
+def main():
+    print(f"Enter the value of a=")
+    a=input_number()
+    print(f"Enter the value of b=")
+    b=input_number()
+    summation=add(a,b)
+    display(a,b,summation)
+
+def input_number():
+    return int(input())
+def add(a,b):
+    return a+b
+def display(a,b,summation):
+    print(f"{a}+{b}={a+b}")
+
+main()
+   
